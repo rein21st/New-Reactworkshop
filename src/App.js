1e9fa7f843f9aa31208e59a6cfa2acbfcc615412
Ben Park <rein21st@gmail.com>
2020-05-27 18:34:30 -0400

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Adding Navigation Bar.
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
 
 class App extends Component {
@@ -12,9 +13,11 @@ class App extends Component {
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
                 </Navbar>
+                <Directory />
             </div>
         );
     }
 }
 
 export default App;
+                    
