65d331c20cdba5f16fd7596870e59368bdb519c0
Ben Park <rein21st@gmail.com>
2020-06-05 16:28:45 -0400

    React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 // Adding Navigation Bar.
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Main />
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>
         );
     };
 }
 // Removed the state-related information in App.js
 export default App;
+
                     
